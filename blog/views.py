cd2141bd1b289fe58c5c8f41505dafe4fefa5679
Verevkin-A <sandyverevkin@gmail.com>
2022-06-13 15:05:33 +0200

    feat: bootstrap support & parent template

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Verevkin",
+        "title": "First post",
+        "content": "First post content",
+        "date_posted": "June 13, 2022"
+    },
+    {
+        "author": "Alex",
+        "title": "Second post",
+        "content": "Some interesting content",
+        "date_posted": "June 13, 2022"
+    }
+]
+
 
 def home(request) -> HttpResponse:
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request) -> HttpResponse:
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "About"})
