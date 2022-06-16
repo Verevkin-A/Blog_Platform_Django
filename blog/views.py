f1761036105823a10e7b581c88123f49ada121bc
Verevkin-A <sandyverevkin@gmail.com>
2022-06-16 19:32:25 +0200

    feat: updated main page with class view

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
 
-def home(request) -> HttpResponse:
-    context = {
-        "posts": Post.objects.all()
-    }
-    return render(request, "blog/home.html", context)
+# class view
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
 
 
+# function view
 def about(request) -> HttpResponse:
-    return render(request, "blog/about.html", {"title": "About"})
+    context = {
+        "title": "About"
+    }
+    return render(request, "blog/about.html", context)
